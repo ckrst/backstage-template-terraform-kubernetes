e77e452874b2cbbba3e1fa0fb5c6bd98aaccf793
Vinicius Kirst <vinicius.kirst@gmail.com>
2021-07-24 21:58:50 -0300

    initial structures

---
+++ skeleton/main.tf
@@ -0,0 +1,42 @@
+# {{cookiecutter.name}}
+
+provider "kubernetes" {
+  config_path = var.kube_config_path
+  config_context = var.kube_config_context
+  config_context_cluster = var.kube_config_context_cluster
+}
+
+resource "kubernetes_namespace" "app_namespace" {
+  metadata {
+    name = var.app_name
+  }
+}
+
+resource "kubernetes_ingress" "app_ingress" {
+  metadata {
+    name = "${var.app_name}-ingress"
+    namespace = "${kubernetes_namespace.app_namespace.metadata.0.name}"
+  }
+
+  spec {
+    backend {
+      service_name = "${var.app_name}-app-service"
+      service_port = 80
+    }
+
+    rule {
+      host = "${var.app_name}.${var.my_domain}"
+
+      http {
+        path {
+          backend {
+            service_name = "${var.app_name}-app-service"
+            service_port = 80
+          }
+
+          path = "/"
+        }
+      }
+    }
+  }
+}
